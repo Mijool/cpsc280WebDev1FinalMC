43af302a54ad8d902537b19fc6c8a8710eb87535
Mijool <118408982+Mijool@users.noreply.github.com>
2025-12-11 13:59:16 -0600

    finished

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function loadProject(){
+    const iframe = document.getElementById('proj1Frame');
+    iframe.style.display = 'block';
+}
+//displays or removes the Iframe
+function closeProject(){
+    const iframe = document.getElementById('proj1Frame');
+    iframe.style.display = 'none';
+}
